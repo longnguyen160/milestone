36ff41a0af7623a768f5e0f177543e626134cef7
thanhdat301 <thanhdatcntt301@gmail.com>
2016-12-22 16:39:47 +0700

    Add files via upload

--- client/modules/users/components/Login.jsx
+++ client/modules/users/components/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+require("./login.css");
 class Login extends React.Component {
   render() {
     const {error} = this.props;
@@ -7,35 +8,40 @@ class Login extends React.Component {
       <div id="mainLogin text-center">
           <div id="Card">
               <form className="centerlize">
-                  <div className="alert alert-danger alert-dismissable nomargin">
+                { error ? <div className="alert alert-danger alert-dismissable nomargin">
                       <a href="#" className="close" data-dismiss="alert" aria-label="close">&times;</a>
-                      <strong>Danger!</strong> Example danger message!
-                  </div>
+                      <strong>{error}!</strong>
+                  </div> : ''
+                }
+
+
+
                   <div className="input-group">
                       <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i></span>
-                      <input id="email" type="text" className="form-control" name="email" placeholder="Email" ref="email"/>
+                      <input ref="email" id="email" type="text" className="form-control" name="email" placeholder="Email" />
                   </div>
                   <div className="input-group">
                       <span className="input-group-addon"><i className="glyphicon glyphicon-lock"></i></span>
-                      <input id="password" type="password" className="form-control" name="password" placeholder="Password" ref="password" />
+                      <input ref="password" id="password" type="password" className="form-control" name="password" placeholder="Password" />
                   </div>
                   <div className="text-center">
-                      <button type="submit" className="btn btn-default">Submit</button>
+                      <button onClick={this.loginUser.bind(this)} type="submit" className="btn btn-default">Submit</button>
                   </div>
                   <a href="/account/forgot" id="forgotpw" >I forgot my password</a>
               </form>
           </div>
           <div id="sigup_mess">
-              New to SI? Sign up as a <a href="#">&nbsp;Broadcaster </a>&nbsp;or&nbsp;<a href="#"> Freelancer </a>
+              New to SI  Sign up as a <a href="#"> Broadcaster </a> or <a href="#"> Freelancer </a>
           </div>
-      </div>
-    )
-  };
-  loginUser(e) {
-    e.preventDefault();
-    const {loginUser} = this.props;
-    const {email,password} = this.refs;
-    loginUser(email.value, password.value);
-  };
-}
+        </div>
+        )
+      };
+      loginUser(e) {
+        e.preventDefault();
+        const {login} = this.props;
+        const {email,password} = this.refs;
+        login(email.value, password.value);
+    };
+    };
+
 export default Login;
