197fce657a27583c787ea1d07b659db217683a8a
thanhdat301 <thanhdatcntt301@gmail.com>
2016-12-22 15:29:01 +0700

    loginUpdate

--- client/modules/users/components/Login.jsx
+++ client/modules/users/components/Login.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 class Login extends React.Component {
   render() {
     const {error} = this.props;
+    console.log(error);
     return (
       <div className="panel panel-default">
+        <p>{error}</p>
         <div className="panel-body">
-          <inpput ref="email" type="text" className="form-control" placeholder="Email address"/>
+          <input ref="email" type="text" className="form-control" placeholder="Email address"/>
           <input ref="password" type="password" className="form-control" placeholder="Password"/>
-          <button onlick=""type="button" className="btn btn-default navbar-btn">Login</button>
+          <button onClick={this.loginUser.bind(this)} type="button" className="btn btn-default navbar-btn">Login</button>
         </div>
       </div>
     )
-  }
-  login(e) {
+  };
+  loginUser(e) {
     e.preventDefault();
     const {loginUser} = this.props;
     const {email,password} = this.refs;
     loginUser(email.value, password.value);
-  }
+  };
 }
 export default Login;
