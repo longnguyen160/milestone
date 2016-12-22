8aecf13474f05bfbea17a052916c480037bcbd6b
longnguyen160 <longnguyen160@gmail.com>
2016-12-22 16:30:38 +0700

    fixed

--- client/modules/users/components/Login.jsx
+++ client/modules/users/components/Login.jsx
@@ -4,13 +4,30 @@ class Login extends React.Component {
     const {error} = this.props;
     console.log(error);
     return (
-      <div className="panel panel-default">
-        <p>{error}</p>
-        <div className="panel-body">
-          <input ref="email" type="text" className="form-control" placeholder="Email address"/>
-          <input ref="password" type="password" className="form-control" placeholder="Password"/>
-          <button onClick={this.loginUser.bind(this)} type="button" className="btn btn-default navbar-btn">Login</button>
-        </div>
+      <div id="mainLogin text-center">
+          <div id="Card">
+              <form className="centerlize">
+                  <div className="alert alert-danger alert-dismissable nomargin">
+                      <a href="#" className="close" data-dismiss="alert" aria-label="close">&times;</a>
+                      <strong>Danger!</strong> Example danger message!
+                  </div>
+                  <div className="input-group">
+                      <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i></span>
+                      <input id="email" type="text" className="form-control" name="email" placeholder="Email" ref="email"/>
+                  </div>
+                  <div className="input-group">
+                      <span className="input-group-addon"><i className="glyphicon glyphicon-lock"></i></span>
+                      <input id="password" type="password" className="form-control" name="password" placeholder="Password" ref="password" />
+                  </div>
+                  <div className="text-center">
+                      <button type="submit" className="btn btn-default">Submit</button>
+                  </div>
+                  <a href="/account/forgot" id="forgotpw" >I forgot my password</a>
+              </form>
+          </div>
+          <div id="sigup_mess">
+              New to SI? Sign up as a <a href="#">&nbsp;Broadcaster </a>&nbsp;or&nbsp;<a href="#"> Freelancer </a>
+          </div>
       </div>
     )
   };
