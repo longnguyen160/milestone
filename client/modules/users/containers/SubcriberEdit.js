c6accc1f0aa53d986748b6f784f41842d52a6e6f
thanhdat301 <thanhdatcntt301@gmail.com>
2016-12-26 11:05:02 +0700

    Update SubcriberEdit.js

--- client/modules/users/containers/SubcriberEdit.js
+++ client/modules/users/containers/SubcriberEdit.js
@@ -1,20 +1,20 @@
-import SubcriberEdit from '../components/SubcriberEdit.jsx';
-import {useDeps,composeWithTracker,composeAll} from 'mantra-core';
+//import SubcriberEdit from '../components/SubcriberEdit.jsx';
+//import {useDeps,composeWithTracker,composeAll} from 'mantra-core';
 
-export const composer = ({context,clearErrors},onData) => {
-    const {LocalState} = context();
-    const error = LocalState.get('SAVE_ERROR');
-    onData(null,{error});
-    return clearErrors;
-};
+//export const composer = ({context,clearErrors},onData) => {
+  //  const {LocalState} = context();
+    //const error = LocalState.get('SAVE_ERROR');
+    //onData(null,{error});
+    //return clearErrors;
+//};
 
-export const depsMapper = (context,actions) => ({
-    edit: actions.users.eddit,
-    clearErrors: actions.users.clearErrors,
-    context: () => context
-});
+//export const depsMapper = (context,actions) => ({
+  //  edit: actions.users.eddit,
+    //clearErrors: actions.users.clearErrors,
+    //context: () => context
+//});
 
-export default composeAll(
-    composeWithTracker(composer),
-    useDeps(depsMapper)
-)(SubcriberEdit);
+//export default composeAll(
+  //  composeWithTracker(composer),
+    //useDeps(depsMapper)
+//)(SubcriberEdit);
