67f4d4fa639acd69508e27d52329722c90179700
thanhdat301 <thanhdatcntt301@gmail.com>
2016-12-26 10:43:47 +0700

    Add files via upload

---
+++ client/modules/users/containers/CompanyRegister.js
@@ -0,0 +1,28 @@
+import CompanyRegister from '../components/CompanyRegister.jsx';
+import {useDeps,composeWithTracker,composeAll} from 'mantra-core';
+export const composer = ({context,clearErrors},onData) => {
+  const {LocalState} = context();
+  const error = new Array();
+  error[0] = LocalState.get("SIGNUP_COMPANY_FIRSTNAME");
+  error[1] = LocalState.get("SIGNUP_COMPANY_LASTNAME");
+  error[2] = LocalState.get("SIGNUP_COMPANY_COMPANY");
+  error[3] = LocalState.get("SIGNUP_COMPANY_EMAIL");
+  error[4] = LocalState.get("SIGNUP_COMPANY_PASSWORD");
+  error[5] = LocalState.get('SIGNUP_COMPANAY_CHECKBOX');
+  if (error[0] && error[1] && error[2] && error[3] && error[4] && error[5])
+    error[6] = true;
+ else
+    error[6] = false;
+  onData(null,{error});
+  return clearErrors;
+};
+export const depsMapper = (context,actions) => ({
+  create: actions.users.createUserCompany,
+  checkValidation: actions.users.checkValidation,
+  clearErrors: actions.users.clearErrors,
+  context: () => context
+});
+export default composeAll(
+  composeWithTracker(composer),
+  useDeps(depsMapper)
+)(CompanyRegister);
